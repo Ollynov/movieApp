a78e48f7342b753167ee83293ccd5aa7dd171ea0
Aljosha Novakovic <ollynov@gmail.com>
2016-08-29 20:22:35 -0700

    Re-structuring app

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,31 @@ var gulp = require('gulp'),
 var src = './process',
     app = './compiled';
 
-gulp.task('js', function() {
-	return gulp.src( src + '/components/app.jsx' )
-	  .pipe(browserify({
-	  	transform: 'reactify',
-	  	debug: true
-	  }))
-	  .on('error', function(err) {
-	  	console.error('gulp task js is picking up error: ', err.message);
-	  })
-	  .pipe(gulp.dest(app + '/js'));
-}); // Compiling everything from the process folder into the compiled folder (converting jsx)
+// gulp.task('js', function() {
+// 	return gulp.src( src + '/components/app.jsx' )
+// 	  .pipe(browserify({
+// 	  	transform: ['reactify'],
+// 	  	debug: true
+// 	  }))
+// 	  .on('error', function(err) {
+// 	  	console.error('gulp task js is picking up error on react compile: ', err.message);
+// 	  })
+// 	  .pipe(gulp.dest(app + '/js'));
+// }); // Compiling everything from the process folder into the compiled folder (converting jsx)
 // Notice how only the single components/app.js file is being compiled. Is this ok? I guess it grabs the other files because they are required?
 // Need to find out how frequently this needs to be run... once on each default task call? How does it pick up changes?
+
+// gulp.task('js-babel', function() {
+//   return gulp.src( src + '/components/app.jsx' )
+//     .pipe(browserify({
+//       transform: ['babelify'],
+//       debug: true
+//     }))
+//     .on('error', function(err) {
+//       console.error('gulp task js is picking up error on babelify compile: ', err.message);
+//     })
+//     .pipe(gulp.dest(app + '/js'));
+// });
 
 gulp.task('html', function() {
 	gulp.src( app + '/**/*.html')
@@ -27,7 +39,7 @@ gulp.task('html', function() {
 // What does the /**/* represent here?
 
 gulp.task('watch', function() {
-  gulp.watch( src + '/js/**/*', ['js']);
+  gulp.watch( src + '/components/**/*', ['js']);
   gulp.watch( app + '/css/**/*.css', ['css']);
   gulp.watch( app + '/js/**/*.js', ['js']);
   gulp.watch( app + '/**/*.html', ['html']);
@@ -41,6 +53,9 @@ gulp.task('webserver', function() {
   	open: true
   }));
 }); // Live running server which will pick up changes just like nodemon. Running on port:
+
+
+
 
 gulp.task('default', ['html', 'watch', 'webserver'])
 
--- process/data/data.js
+++
@@ -1,8 +0,0 @@
-var data = [
- {
-	 title: 'Batman',
-	 rating: 10,
-	 image: 'http://screencrave.com/wp-content/uploads/2012/07/Batman-The-Dark-Knight-Rises.jpg'
- }
-
-];
